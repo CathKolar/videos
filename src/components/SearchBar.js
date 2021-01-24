073d5b6db19e20dad5f1adf7d02a76b5a738ef00
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-01-24 13:32:22 +0100

    create and style SearchBar form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,20 @@ import React from "react";
 
 class SearchBar extends React.Component {
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              autoFocus
+              type="text"
+              placeholder="Whatcha wanna watch!?"
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 export default SearchBar;
