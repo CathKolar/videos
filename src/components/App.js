83e39e4a809d747ce52577a716f7b46a0f4ac65a
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-01-25 20:56:46 +0100

    style App.js
    add margin above search bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
+import "./App.css";
 import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="ui container">
+      <div className="App ui container">
         <SearchBar />
       </div>
     );
