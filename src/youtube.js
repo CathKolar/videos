99837dcaccb1df41ee928794ff4add046da36b94
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-01-26 18:33:37 +0100

    install axios, create youtube.js to handle youtube API call /axios configuration

---
+++ src/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyDveeCgWbYg9wd8QRs63Ctq1hxs8aI6uic";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  },
+});
