b191af490a129107fa4fbaebb2e8ca0c5dd4e8b1
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-01-27 21:24:01 +0100

    move youtube.js to apis folder, import youtube.js to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
   onTermSubmit = (term) => {};
