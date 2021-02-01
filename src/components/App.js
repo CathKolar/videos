b7843c336af25946270047c9f13092a3084e9d3b
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-02-01 12:32:58 +0100

    create and render VideoDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,11 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
@@ -15,13 +18,14 @@ class App extends React.Component {
   };
 
   onVideoSelect = (video) => {
-    console.log("From the App!", video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="App ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
